a3e30ad1aad492f3969069003d97ca8e29829059
Bruno de Moura <brunomoura0509@hotmail.com>
2024-10-07 18:39:54 -0300

    Creating the project and configuring it to use ViewCode.

---
+++ FlightHub/FlightHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlightHub
+//
+//  Created by Bruno Moura on 07/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
